92c471132eece0ec72440a7a887bb94d59ecd85f
renxia <renxia@users.noreply.github.com>
2023-03-21 14:28:25 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/MessageAction.tsx
@@ -0,0 +1,53 @@
+import { createSignal, Show, Switch, Match } from "solid-js"
+import type { Role } from "~/types"
+export default function MessageAction(props: {
+  role: Role
+  hidden: boolean
+  edit: () => void
+  del: () => void
+  copy: () => void
+}) {
+  const [copied, setCopied] = createSignal(false)
+  return (
+    <Show when={!props.hidden}>
+      <div class="flex absolute items-center justify-between sm:top-2 sm:right-2 top--2 right-0 text-sm text-slate-7 dark:text-slate group-hover:opacity-100 group-focus:opacity-100 opacity-0 bg-slate bg-op-10 rounded">
+        <Switch>
+          <Match when={props.role === "assistant"}>
+            <ActionItem
+              label="复制"
+              onClick={() => {
+                setCopied(true)
+                props.copy()
+                setTimeout(() => setCopied(false), 2000)
+              }}
+              icon={copied() ? "i-un:copied" : "i-un:copy"}
+            />
+          </Match>
+          <Match when={props.role === "user"}>
+            <ActionItem
+              label="编辑"
+              onClick={props.edit}
+              icon={"i-carbon:edit"}
+            />
+          </Match>
+        </Switch>
+        <ActionItem
+          label="删除"
+          onClick={props.del}
+          icon={"i-carbon:trash-can"}
+        />
+      </div>
+    </Show>
+  )
+}
+
+function ActionItem(props: { onClick: any; icon: string; label?: string }) {
+  return (
+    <div
+      class="flex items-center cursor-pointer p-2 hover:bg-slate hover:bg-op-20 rounded text-1.2em"
+      onClick={props.onClick}
+    >
+      <button class={props.icon} title={props.label} />
+    </div>
+  )
+}
---
+++ src/components/MessageItem.tsx
@@ -0,0 +1,108 @@
+import type { Setter } from "solid-js"
+import type { ChatMessage, Role } from "../types"
+import MarkdownIt from "markdown-it"
+// @ts-ignore
+import mdKatex from "markdown-it-katex"
+import mdHighlight from "markdown-it-highlightjs"
+import mdKbd from "markdown-it-kbd"
+import MessageAction from "./MessageAction"
+import { preWrapperPlugin } from "~/markdown-it"
+import "../styles/message.css"
+import "../styles/clipboard.css"
+import { useCopyCode } from "~/hooks"
+import { copyToClipboard } from "~/utils"
+import vercel from "/assets/vercel.svg?raw"
+import openai from "/assets/openai.svg?raw"
+
+interface Props {
+  role: Role
+  message: string
+  index?: number
+  setInputContent?: Setter<string>
+  setMessageList?: Setter<ChatMessage[]>
+}
+
+export default (props: Props) => {
+  useCopyCode()
+  const roleClass = {
+    error: "bg-gradient-to-r from-red-400 to-red-700",
+    system: "bg-gradient-to-r from-gray-300 via-gray-200 to-gray-300",
+    user: "bg-gradient-to-r from-red-300 to-blue-700 ",
+    assistant: "bg-gradient-to-r from-yellow-300 to-red-700 "
+  }
+
+  const md = MarkdownIt({
+    linkify: true,
+    breaks: true
+  })
+    .use(mdKatex)
+    .use(mdHighlight, {
+      inline: true
+    })
+    .use(mdKbd)
+    .use(preWrapperPlugin)
+
+  function copy() {
+    copyToClipboard(props.message)
+  }
+
+  function edit() {
+    props.setInputContent && props.setInputContent(props.message)
+  }
+
+  function del() {
+    if (props.setMessageList && props.index !== undefined) {
+      props.setMessageList(list => {
+        if (list[props.index!]?.role === "user") {
+          const arr = list.reduce(
+            (acc, cur, i) => {
+              if (cur.role !== "user" && i === acc.at(-1)! + 1) acc.push(i)
+              return acc
+            },
+            [props.index] as number[]
+          )
+
+          return list.filter((_, i) => {
+            return !arr.includes(i)
+          })
+        }
+        return list.filter((_, i) => i !== props.index)
+      })
+    }
+  }
+
+  return (
+    <div
+      class="group flex gap-3 px-4 mx--4 rounded-lg transition-colors sm:hover:bg-slate/5 dark:sm:hover:bg-slate/2 relative message-item"
+      classList={{
+        temporary: props.index === undefined
+      }}
+    >
+      <div
+        class={`shrink-0 w-7 h-7 mt-4 rounded-full op-80 ${
+          roleClass[props.role]
+        }`}
+      />
+      <div
+        class="message prose prose-slate dark:prose-invert dark:text-slate break-words overflow-hidden"
+        innerHTML={md
+          .render(props.message)
+          .replace(
+            /Vercel/g,
+            `<a href="http://vercel.com/?utm_source=busiyi&utm_campaign=oss" style="border-bottom:0">${vercel}</a>`
+          )
+          .replace(
+            /OpenAI/g,
+            `<a href="https://www.openai.com" style="border-bottom:0">${openai}</a>`
+          )}
+      />
+      <MessageAction
+        del={del}
+        copy={copy}
+        edit={edit}
+        role={props.role}
+        hidden={props.index === undefined}
+      />
+    </div>
+  )
+}
